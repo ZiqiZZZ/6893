030ee44363de789be763fad67cf5f0ffc72213d9
Li Cai <licai@lis-macbook-pro.lan>
2021-12-16 11:14:03 -0500

    commit

--- src/myapp/synthesize.py
+++ src/myapp/synthesize.py
@@ -1,10 +1,11 @@
 from django.core.files import File
 from .models import Document
 import os
-import time
+import asyncio
 
-def synthesize(file_dir):
-    time.sleep(5)
+
+async def synthesize(file_dir):
+    # await asyncio.sleep(5)
     with open(file_dir, 'rb') as orignal_video:
         name = os.path.basename(orignal_video.name)
         newName = name.split('.')[0]+'_synthesized.mp4'
--- src/myapp/views.py
+++ src/myapp/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import redirect, render
 from .models import Document
 from .forms import DocumentForm
 from .synthesize import synthesize
-import os
+import os, asyncio
+from asgiref.sync import sync_to_async
+import django, asyncio
 
 
-def my_view(request):
+async def my_view(request):
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rest.settings')
+    os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+    django.setup()
+
     print(f"Great! You're using Python 3.6+. If you fail here, use the right version.")
     message = 'You can upload multiple videos!'
     # Handle file upload
@@ -17,7 +23,7 @@ def my_view(request):
 
             base = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
             dir = os.path.join(base, 'media', doc.docfile.name),
-            synthesize(dir[0])
+            await synthesize(dir[0])
 
             # Redirect to the document list after POST
             return redirect('my-view')
